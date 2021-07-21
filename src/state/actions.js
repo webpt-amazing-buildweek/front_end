5547a452523648149983e4a4fe219940a6a7126b
Chris Lau <chrislyc1991@gmail.com>
2021-07-21 15:09:02 -0600

    updated actions.js file

--- src/state/actions.js
+++ src/state/actions.js
@@ -3,13 +3,70 @@
 // import and export all action creators
 // make sure to comment what each does
 
-import { getRecipe } from "./_shared/middleware/api";
+import {
+    postLogIn as _postLogIn,
+    createUser as _createUser,
+    updateUser as _updateUser,
+    createItem as _createItem,
+    updateItem as _updateItem,
+    deleteItem as _deleteItem
+} from "./_shared/middleware/api";
 
 
-export const getRecipes = getRecipe;
-// api call to get a recipe
-// args: props
-// actions: FETCHING_API_START, FETCHING_API_SUCCESS, FETCHING_API_FAILURE
+export const postLogIn = _postLogIn;
+// args:
+// formValues {
+//     name: string;
+//     password: string;
+//   }
+// actions: API_STATUS_CHANGE
+// state changed: api
+export const createUser = _createUser;
+// args:
+// formValues {
+//     name: string;
+//     email: string;
+//     password: string;
+//     isOwner: boolean;
+//   }
+// actions: API_STATUS_CHANGE
+// state changed: api
+export const updateUser = _updateUser;
+// args:
+// user {
+//     name: string;
+//     email: string;
+//     password: string;
+//     isOwner: boolean;
+//   }
+// actions: API_STATUS_CHANGE
+// state changed: api
+export const createItem = _createItem;
+// args:
+// item {
+//     name: string;
+//     description: string;
+//     location: string;
+//     price: number;
+//   }
+// actions: API_STATUS_CHANGE
+// state changed: api
+export const updateItem = _updateItem;
+// args:
+// item {
+//     name: string;
+//     description: string;
+//     location: string;
+//     price: number;
+//   }
+// id: number;
+// actions: API_STATUS_CHANGE
+// state changed: api
+export const updateItem = _updateItem;
+// args:
+// id: number;
+// actions: API_STATUS_CHANGE
+// state changed: api
 
 
 
