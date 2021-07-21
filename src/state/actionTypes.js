5a465cff26e3bec850368dde6e97b60cdbd7fe6a
Chris Lau <chrislyc1991@gmail.com>
2021-07-21 14:51:06 -0600

    deleted unused actionTypes

--- src/state/actionTypes.js
+++ src/state/actionTypes.js
@@ -1,7 +1,2 @@
 // file to store the action type constants
 export const API_STATUS_CHANGE = "API_STATUS_CHANGE";
-
-export const FETCHING_API_START = "FETCHING_API_LOADING";
-export const FETCHING_API_SUCCESS = "FETCHING_API_SUCCESS";
-export const FETCHING_API_FAILURE = "FETCHING_API_FAIL";
-export const SEARCH_VALUE = "SEARCH_VALUE";
