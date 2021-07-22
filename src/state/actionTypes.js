1fb1a48c54ecf550ebb814e6c16d9ee2cea71cc2
Durrell Jules <durrelljules@Durrells-Mini.attlocal.net>
2021-07-21 20:24:57 -0400

    constructed constants for user and items, templete for reducer cases

--- src/state/actionTypes.js
+++ src/state/actionTypes.js
@@ -1,2 +1,11 @@
 // file to store the action type constants
 export const API_STATUS_CHANGE = "API_STATUS_CHANGE";
+
+// user constants
+
+export const UPDATE_USER = "UPDATE_USER";
+
+// item constants 
+
+export const UPDATE_ITEMS = "UPDATE_ITEMS"
+export const DELETE_ITEMS = "DELETE_ITEMS"
