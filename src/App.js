b486632c1f4128bfab293a506fe1dd7c24dfff1d
jlbevans <jacob-bevans@lambdastudents.com>
2021-07-21 19:59:48 -0600

    I have added the sign up form code

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import "tailwindcss/dist/tailwind.css";
 import React from "react";
 import { AppNav, AppHome, AppAbout, AppRecipes, AppChef, AppNutrition, AppCocktail } from "./view/components/index";
+import SignUpForm from './view/components/forms/signupform'
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 
@@ -15,6 +16,7 @@ function App() {
         <Route exact path={"/"} component={AppHome} />
         <Route path={"/about"} component={AppAbout} />
         <Route path={"/chef"} component={AppChef} />
+        <Route path={"/signup"} component={SignUpForm} />
         <Route path={"/recipes"} component={AppRecipes} />
         <Route path={'/nutrition'} component={AppNutrition}/>
         <Route path={'/cocktails'} component={AppCocktail}/>
