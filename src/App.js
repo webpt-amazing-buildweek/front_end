7a9d24d6045cc08347f578606c8414eee6aa1eab
Chris Lau <chrislyc1991@gmail.com>
2021-07-23 15:09:33 -0600

    cleaned up App.js and got rid of the dispatch call tests

--- src/App.js
+++ src/App.js
@@ -1,40 +1,10 @@
 import "./App.css";
 import "tailwindcss/dist/tailwind.css";
-import React , { useEffect }from "react";
+import React from "react";
 import { Nav, Home, User, SignUp } from "./view/components/index";
-import PrivateRoute from "./view/PrivateRoute";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import {useDispatch} from "react-redux";
-import {createUser} from "./state/actions";
-import axios from 'axios';
-import { testResponse } from "./state/_shared/middleware/api";
-// const dummyLogIn = {
-//   username:"chris",
-//   password:"123456789"
-// };
-const dummySignUp = {
-  username:"chris",
-  password:"123456789",
-  email: "cool@cmail.com",
-  isOwner:true
-};
-
 
 function App() {
-  // this code is for testing api. remove later----
-  const dispatch = useDispatch();
-  useEffect(()=>{
-    dispatch(testResponse());
-  },[dispatch])
-  // axios.get("http://localhost:5000")
-  // .then((res)=>{
-  //   console.log("!!!!!!!!!!!!!!!!!!!",res)
-  // })
-  // .catch((err)=>{
-  //   console.log({err});
-  // })
-  // },[])
-  // --------
   return (
     <Router>
       <div className="App">
