cce410fd01cf28d1d738ef46ac86116e372c2305
Chris Lau <chrislyc.lau@gmail.com>
2021-08-01 18:41:30 -0600

    styled search bar

--- src/view/components/Marketplace.js
+++ src/view/components/Marketplace.js
@@ -58,6 +58,7 @@ const Marketplace=(props)=>{
                     <div className={"absolute -inset-y-0 rounded"}>
                         <div>
                             <input 
+                            className={"bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-green-800"}
                             value={searchValue}
                             placeholder="Search..."
                             onChange={handleSearchTerm}
